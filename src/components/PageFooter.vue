8126eff9572b748caf2cd2bb8b99ae00f6bbb874
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-06 16:16:41 +0100

    add-code-of-footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,9 +1,71 @@
-<script setup>
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+
+}
 
 
 
 </script>
 
-<template></template>
+<template>
+    <footer>
+        <div class="container footer-border">
+            <div class="row footer-row">
+                <div class="col-6">
+                    <div class="footer-text">
+                        <strong>Boolando s.r.l</strong>
+                        <p>informazioni legali informativa sulla privacy Diritto di recesso</p>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="footer-text-2">
+                        <strong>Trovaci anche su</strong>
+                        <img src="./img/Senza titolo-1.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+footer {
+    background-color: #1a1a1a;
+    height: 110px;
+    width: 100%;
+    /* position: sticky;
+    z-index: 2000;
+    bottom: 0;*/
+}
+
+.footer-row {
+    justify-content: space-between;
+}
+
+.col-6 {
+    flex-basis: calc((100% / 12) * 6);
+    /* flex-basis: calc((100% * 2) / 12); */
+    /* border: 1px dashed black; */
+}
+
+.footer-border {
+    padding-top: 20px;
+}
+
+.footer-text {
+    color: white;
+    line-height: 25px;
+}
+
+.footer-text-2 {
+    color: white;
+    line-height: 25px;
+    text-align: right;
+}
+</style>
