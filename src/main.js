b3dc5d1f58ab1b026c04fef4af0e22f6ccdc5260
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-08 18:01:22 +0100

    imported-icons

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faHeart, faUser } from '@fortawesome/free-regular-svg-icons'
+import { faBagShopping } from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(faHeart, faUser, faBagShopping);
+
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
