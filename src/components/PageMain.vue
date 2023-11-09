c53f9bb4f59352afc5263aef3e422520d8197579
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-09 16:12:04 +0100

    add-bg-black-on-modal

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -38,8 +38,8 @@ export default {
                 </div>
             </div>
         </div>
-        <div>
-            <div v-if="active" class="card">
+        <div v-if="active" class="bg-modal">
+            <div class="card">
                 <div class="card__body">
                     <figure>
                         <img class="imgModal" :src="items.frontImage" alt="">
@@ -63,22 +63,33 @@ export default {
     flex-basis: calc((100% / 12) * 4);
 }
 
+.bg-modal::after {
+    content: '';
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+    background-color: rgba(0, 0, 0, 0.7);
+}
+
 .card {
     position: fixed;
     top: 50%;
     left: 50%;
-    transform: translate(-50%);
+    transform: translate(-50%, -50%);
     background-color: white;
     box-shadow: 0px 0px 10px 2px #888888;
-    max-width: 450px;
+    max-width: 800px;
     width: 100%;
-    height: 200px;
+    height: 500px;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
     padding: 10px;
-    align-items: center;
     border-radius: 10px;
+    z-index: 2;
 
     .card__body {
         display: flex;
@@ -89,10 +100,11 @@ export default {
         padding: 10px;
         font-weight: bold;
         gap: 10px;
+        font-size: 25px;
 
         .imgModal {
             display: block;
-            width: 100px;
+            width: 300px;
             border-radius: 10px;
             box-shadow: 0px 0px 10px 2px #888888;
 
